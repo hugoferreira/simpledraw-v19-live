4be1ccb3990cd65e27dda10e7b8fe5b9914d8ca0
Xavier Fontes <xfontes42@gmail.com>
2019-03-13 09:58:31 +0000

    cleaned comments

--- src/actions.ts
+++ src/actions.ts
@@ -68,23 +68,15 @@ export class CreateSelectionAction {
 }
 
 
-        
-
 export class TranslateAction implements Action<void> {
-    oldX: number
-    oldY: number
 
     constructor(private doc: SimpleDrawDocument, public shape: Shape, private xd: number, private yd: number) { }
 
     do(): void {
-        // this.oldX = this.shape.x
-        // this.oldY = this.shape.y
         this.shape.translate(this.xd, this.yd)
     }
 
     undo() {
-        // this.shape.x = this.oldX
-        //this.shape.y = this.oldY
         this.shape.translate(-this.xd, -this.yd)
     }
 }
