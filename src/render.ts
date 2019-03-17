d10be6b30fd7a4ce48bbc0fbb154243515cf3a10
darksystem <diogo.rey@hotmail.com>
2019-03-17 17:24:07 +0000

    Added linters

--- src/render.ts
+++ src/render.ts
@@ -1,47 +1,47 @@
-import { Shape, Circle, Rectangle } from "./shape"
+import { Circle, Rectangle, Shape } from './shape'
 
 export interface Render {
-    draw(...objs: Array<Shape>): void
+  draw (...objs: Shape[]): void
 }
 
 export class SVGRender implements Render {
-    svg: HTMLElement
+  public svg: HTMLElement
 
-    constructor() {
-        this.svg = <HTMLElement>document.getElementById('svgcanvas')
-    }
+  constructor () {
+    this.svg = document.getElementById('svgcanvas') as HTMLElement
+  }
 
-    draw(...objs: Array<Shape>): void {
-        for (const shape of objs) {
-            if (shape instanceof Rectangle) {
-                const e = document.createElementNS("http://www.w3.org/2000/svg", "rect")
-                e.setAttribute('style', 'stroke: black; fill: white')
-                e.setAttribute('x', shape.x.toString())
-                e.setAttribute('y', shape.y.toString())
-                e.setAttribute('width', shape.width.toString())
-                e.setAttribute('height', shape.height.toString())
-                this.svg.appendChild(e)
-            }
-        }
+  public draw (...objs: Shape[]): void {
+    for (const shape of objs) {
+      if (shape instanceof Rectangle) {
+        const e = document.createElementNS('http://www.w3.org/2000/svg', 'rect')
+        e.setAttribute('style', 'stroke: black; fill: white')
+        e.setAttribute('x', shape.x.toString())
+        e.setAttribute('y', shape.y.toString())
+        e.setAttribute('width', shape.width.toString())
+        e.setAttribute('height', shape.height.toString())
+        this.svg.appendChild(e)
+      }
     }
+  }
 }
 
 export class CanvasRender implements Render {
-    ctx: CanvasRenderingContext2D
+  public ctx: CanvasRenderingContext2D
 
-    constructor() {
-        const canvas = <HTMLCanvasElement>document.getElementById('canvas')
-        this.ctx = canvas.getContext('2d')
-    }
+  constructor () {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement
+    this.ctx = canvas.getContext('2d')
+  }
 
-    draw(...objs: Array<Shape>): void {
-        for (const shape of objs) {
-            if (shape instanceof Circle) {
-                this.ctx.ellipse(shape.x, shape.y, shape.radius, shape.radius, 0, 0, 2 * Math.PI)
-                this.ctx.stroke()
-            } else if (shape instanceof Rectangle) {
-                this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)   
-            }
-        }
+  public draw (...objs: Shape[]): void {
+    for (const shape of objs) {
+      if (shape instanceof Circle) {
+        this.ctx.ellipse(shape.x, shape.y, shape.radius, shape.radius, 0, 0, 2 * Math.PI)
+        this.ctx.stroke()
+      } else if (shape instanceof Rectangle) {
+        this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+      }
     }
+  }
 }
